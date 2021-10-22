000b3bd40483532a48c52863fc70724772974398
elsyelins <sihombing.elisabet55@gmail.com>
2021-10-22 17:52:45 +0700

    Java Basic 5

---
+++ Hero.java
@@ -0,0 +1,60 @@
+public class Hero extends Human {
+
+    private int energy = 100;
+
+    public Hero(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void player() {
+        System.out.println("============Player============");
+        System.out.println("Name   : " + this.name);
+        System.out.println("Age    : " + this.age);
+        System.out.println("Energy : " + this.energy);
+        System.out.println("==============================");
+    }
+
+    void checkEnergy() {
+        if(energy <= 0) {
+            System.out.println("Energy Anda Habis! Player " + this.name + " Tamat!");
+            System.exit(0);
+        }
+    }
+
+    void run() {
+        checkEnergy();
+        System.out.println(this.name + " sedang berlari...");
+        energy -= 10;
+    }
+
+    void eat() {
+        checkEnergy();
+        System.out.println(this.name + " sedang makan...");
+        energy += 20;
+    }
+
+    void drink(){
+        checkEnergy();
+        System.out.println(this.name + " sedang minum...");
+        energy += 20;
+    }
+
+    void jump(){
+        checkEnergy();
+        System.out.println(this.name + " sedang melompat...");
+        energy -= 20;
+    }
+
+    void sit(){
+        checkEnergy();
+        System.out.println(this.name + " sedang duduk...");
+        energy += 10;
+    }
+
+    void attack(){
+        checkEnergy();
+        System.out.println(this.name + " sedang menyerang...");
+        energy -= 30;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,56 @@
+class Main {
+    public static void main(String[] args) {
+        Hero hero = new Hero("Elizabeth Yelin Melani", 20);
+
+        hero.player();
+
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.run();
+        hero.eat();
+        hero.eat();
+        hero.eat();
+        hero.eat();
+        hero.drink();
+        hero.drink();
+        hero.jump();
+        hero.jump();
+        hero.jump();
+        hero.jump();
+        hero.jump();
+        hero.jump();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.walk();
+        hero.sit();
+        hero.sit();
+        hero.sit();
+        hero.sit();
+        hero.sit();
+        hero.attack();
+
+        hero.player();
+    }
+}
